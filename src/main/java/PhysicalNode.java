1fc3c630ff785ee75d31ea8b1808982e007427a0
Chouiter Skander <chouites@im2ag-mandelbrot2.u-ga.fr>
2024-03-20 09:39:00 +0100

    ajout de rabbitmq et de queue entre chaque connection de noeuds physique

--- src/main/java/PhysicalNode.java
+++ src/main/java/PhysicalNode.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import com.rabbitmq.client.*;
 
 public class PhysicalNode {
 
@@ -6,10 +7,17 @@ public class PhysicalNode {
     private ArrayList<Integer> neighbors;
     private ArrayList<String> nodeInformation;
 
-    public PhysicalNode(int nodeID) {
+    // RabbitMQ
+    private Connection connection;
+    private Channel channel;
+
+    public PhysicalNode(int nodeID) throws Exception{
         this.nodeID = nodeID;
         neighbors = new ArrayList<Integer>();
         nodeInformation = new ArrayList<String>();
+
+        // Setup RabbitMQ Connection
+        setupRabbitMQ();
     }
 
     public int getNodeID() {
@@ -32,31 +40,57 @@ public class PhysicalNode {
         this.nodeInformation = nodeInformation;
     }
 
+        private void setupRabbitMQ() throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        // Set necessary connection properties, e.g., host
+        factory.setHost("localhost");
+        connection = factory.newConnection();
+        channel = connection.createChannel();
+    }
+
+    public void createQueuesForNeighbors() throws Exception {
+        for (Integer neighborID : neighbors) {
+            String queueName = formatQueueName(nodeID, neighborID);
+            channel.queueDeclare(queueName, false, false, false, null);
+            System.out.println("Queue Created: " + queueName);
+        }
+    }
+
+    private String formatQueueName(int node1, int node2) {
+        if (node1 < node2) {
+            return "queue_" + node1 + "_" + node2;
+        } else {
+            return "queue_" + node2 + "_" + node1;
+        }
+    }
+
     public ArrayList<String> getNodeInformation() {
         return nodeInformation;
     }
 
+   
     public static void main(String[] args) {
         if (args.length == 0) {
             System.exit(-1);
         }
 
-        PhysicalNode n = new PhysicalNode(Integer.parseInt(args[0]));
-        ArrayList<String> nodeInformation = MatrixReader.getNodeInformation(n.getNodeID());
-        n.setNodeInformation(nodeInformation);
+        try {
+            PhysicalNode n = new PhysicalNode(Integer.parseInt(args[0]));
+            ArrayList<String> nodeInformation = MatrixReader.getNodeInformation(n.getNodeID());
+            n.setNodeInformation(nodeInformation);
 
-        for (int i = 0; i < nodeInformation.size(); i++) {
-            if (Integer.parseInt(nodeInformation.get(i)) == 1) {
-                n.addNeighbor(i);
+            for (int i = 0; i < nodeInformation.size(); i++) {
+                if (Integer.parseInt(nodeInformation.get(i)) == 1) {
+                    n.addNeighbor(i);
+                }
             }
+
+            n.createQueuesForNeighbors();
+
+            System.out.println(n.getNodeInformation());
+            System.out.println(n.getNeighbors());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        System.out.println(n.getNodeInformation());
-        System.out.println(n.getNeighbors());
-
-        // for each neighbor, we will create a queue dynamically,
-        // using the format (queue_smallerNumber_greaterNumber)
-
-
     }
 }
